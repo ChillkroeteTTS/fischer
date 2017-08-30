3bd2aed29e820477f20183095dfa23d7be4789c1
Tjark Smalla <tjark.smalla@otto.de>
2017-08-30 15:29:05 +0200

    add file reporter

---
+++ src/conan/reporter/file_reporter.clj
@@ -0,0 +1,11 @@
+(ns conan.reporter.file-reporter
+  (:require [conan.reporter.prediction-reporter :as r]
+            [clj-time.core :as t]))
+
+(defrecord FileReporter [file-path]
+  r/PredictionReporter
+  (report [self profile->prediction]
+    (with-open [w (clojure.java.io/writer file-path :append true)]
+      (.write w (str "-------- " (str (t/now)) "--------\n"))
+      (doseq [[profile prediction] profile->prediction]
+        (.write w (str profile ": " (:s prediction) " --(" (:e prediction) ")--> " (:p prediction) "\n"))))))
---
+++ test/conan/reporter/file_reporter_test.clj
@@ -0,0 +1,16 @@
+(ns conan.reporter.file-reporter-test
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [conan.reporter.file-reporter :as fr]
+            [conan.reporter.prediction-reporter :as r]
+            [clj-time.core :as t]))
+
+(def test-file-path "test-resources/predictions")
+
+(deftest file-reporter-test
+  (testing "it writes the predictions to a file"
+    (with-redefs [t/now (constantly (t/date-time 2017 01 01 00 00 00))]
+      (r/report (fr/->FileReporter test-file-path) {:profile1 {:e 0.02 :s 0.324 :p false}})
+      (is (= "-------- 2017-01-01T00:00:00.000Z--------\n:profile1: 0.324 --(0.02)--> false\n"
+             (slurp test-file-path)))
+      (io/delete-file test-file-path))))
