7a7fff4d0a029e55f9a9dcc0bde3ec88b8314bb2
Tjark Smalla <tjark.smalla@otto.de>
2017-08-30 16:07:47 +0200

    clean up project-clj again...

--- project.clj
+++ project.clj
@@ -7,8 +7,6 @@
                  [incanter "1.9.1" :exclusions [org.clojure/clojure]]
                  [clj-http "3.6.1"]
                  [clojure-future-spec "1.9.0-alpha17"]
-                 [de.otto/tesla-microservice "0.11.3"]
-                 [de.otto.tesla/basic-logging "0.3.1"]
                  [org.clojure/data.json "0.2.6"]
                  [com.outpace/config "0.10.0"]]
   :aliases {"config" ["run" "-m" "outpace.config.generate"]})
