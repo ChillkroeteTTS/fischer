b162ca858177e86d35424d0f447898c2e4b9f70e
tsmalla <tjark.smalla@gmx.de>
2017-09-25 21:30:05 +0200

    Fix broken test for PrometheusReporter

--- test/conan/reporter/PrometheusReporterTest.clj
+++ test/conan/reporter/PrometheusReporterTest.clj
@@ -8,10 +8,10 @@
 
 (deftest PrometheusReporter-test
   (testing "it sets the anomaly detected metric to the correct value"
-    (r/report (promr/->PrometheusReporter) {:profile1 {:p 1.0
+    (r/report (promr/->PrometheusReporter) {:profile1 {:p true
                                                        :s 0.01
                                                        :e 0.02}
-                                            :profile2 {:p 0.0
+                                            :profile2 {:p false
                                                        :s 0.03
                                                        :e 0.02}})
     (is (= 1.0 (.get ((goo/snapshot) :conan/anomaly-detected {:profile "profile1"}))))
